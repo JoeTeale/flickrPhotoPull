01db4c1715217ecb2729742ae0ebad7153902a74
Joseph Teale <mrjoeteale@gmail.com>
2018-12-01 18:51:26 +0000

    Playing around with Grids e.t.c

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-let selectedColours = ['red', 'blue'];
-selectedColours[2] = false
-console.log(selectedColours);
-
-
-function greet(name, lastName) {
-  return ('hello ' + name + ' ' + lastName);
-}
-
-console.log(greet('Joe', "Teale"));
 let apiKey='api_key=e4dd1d06d77be6c00f9ff5b3db546260';
-let tag = 'tags=cats';
+let tag = 'tags=dogs';
 let format = 'format=json&nojsoncallback=1';
 let begginingHTTP = 'https://api.flickr.com/services/rest/?'
 let url = `${begginingHTTP}method=flickr.photos.search&${apiKey}&${tag}&${format}`;
@@ -31,12 +21,11 @@ function imageRender(response) {
   response.photos.photo.forEach(element => {
     let image = document.getElementById("myImg");
     let imageUrl = `http://farm${element.farm}.staticflickr.com/${element.server}/${element.id}_${element.secret}.jpg`;
-    let imgStyle = document.createElement('img');
-      imgStyle.src = imageUrl; 
-      imgStyle.width=200;
-      imgStyle.height=100;
-        console.log(imgStyle);
-        document.body.appendChild(imgStyle);
+    let divTag = document.createElement('div');
+    let flickrImage = document.createElement('img');
+      flickrImage.src = imageUrl; 
+        console.log(flickrImage);
+        document.body.appendChild(divTag).appendChild(flickrImage);
     console.log(image);
 });
 
